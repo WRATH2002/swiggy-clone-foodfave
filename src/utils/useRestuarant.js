082a386873ab12c7d19a370dcffb3396ee1f40e7
Himadri Purkait <purkaithimadricollege@gmail.com>
2023-09-08 21:21:55 +0530

    10th commit

--- src/utils/useRestuarant.js
+++ src/utils/useRestuarant.js
@@ -30,7 +30,7 @@ export const useFilteredRestuarants = () => {
 
   async function getrestuarants() {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.4875917&lng=88.3711233&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.4875917&lng=88.3711233&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     console.log(json);
